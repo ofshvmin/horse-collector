32b6ebec5de524ce780131fa51a9756d46275adb
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-09-06 14:02:30 -0400

    add Horse model

--- main_app/views.py
+++ main_app/views.py
@@ -2,22 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-class Horse:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-horses = [
-  Horse('Tater Tot', 'Haflinger', 'A big potato who enjoys his job as a trail horse.', 21),
-  Horse('Aspen', 'Appaloosa', 'A demure southern lady with refined taste in riders.  Sorry folks, she is not for just anyone.  Aspen likes jumping and foxhunting best.', 14),
-  Horse('Morgan le Fay', 'Morgan/Percheron', 'A sweet girl just starting her career as an endurance champion.', 7)
-]
+# horses = [
+#   Horse('Tater Tot', 'Haflinger', 'A big potato who enjoys his job as a trail horse.', 21),
+#   Horse('Aspen', 'Appaloosa', 'A demure southern lady with refined taste in riders.  Sorry folks, she is not for just anyone.  Aspen likes jumping and foxhunting best.', 14),
+#   Horse('Morgan le Fay', 'Morgan/Percheron', 'A sweet girl just starting her career as an endurance champion.', 7)
+# ]
 
 # Create your views here.
 def home(request):
-  return render(request, 'base.html')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
