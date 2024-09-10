203d5129685d65a69ab261fd6346f3cb726bf21d
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-09-10 11:02:51 -0400

    implement update and delete horse functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('horses/', views.horse_index, name='horse-index'),
   path('horses/<int:horse_id>/', views.horse_detail, name='horse-detail'),
   path('horses/create/', views.HorseCreate.as_view(), name='horse-create'),
+  path('horses/<int:pk>/update/', views.HorseUpdate.as_view(), name='horse-update'),
+  path('horses/<int:pk>/delete/', views.HorseDelete.as_view(), name='horse-delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Horse
 
 
@@ -21,3 +21,11 @@ def horse_detail(request, horse_id):
 class HorseCreate(CreateView):
   model = Horse
   fields = '__all__'
+
+class HorseUpdate(UpdateView):
+  model = Horse
+  fields = ['breed', 'description', 'age']
+
+class HorseDelete(DeleteView):
+  model = Horse
+  success_url = '/horses/'
