fa063854aa3de1a4d3ddbbf435e49d07d930fbaa
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-09-10 12:19:39 -0400

    add FarrierAppt model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Horse
+from .models import Horse, FarrierAppt
 
 # Register your models here.
 admin.site.register(Horse)
+admin.site.register(FarrierAppt)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+
+SERVICES = ( 
+  ('T', 'A Hoof Trim'),
+  ('S', 'Shoes'),
+  ('B', 'A Hoof Trim and Shoes')
+)
+
 # Create your models here.
 class Horse(models.Model):
   name = models.CharField(max_length=100)
@@ -13,5 +20,15 @@ class Horse(models.Model):
 
   def get_absolute_url(self):
     return reverse("horse-detail", kwargs={"horse_id": self.id})
-  
 
+class FarrierAppt(models.Model):
+  date =  models.DateField('Appointment Date')
+  service = models.CharField(
+    max_length=1,
+    choices=SERVICES,
+    default=SERVICES[0][0]
+  )
+  horse = models.ForeignKey(Horse, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_service_display()} on {self.date}"
